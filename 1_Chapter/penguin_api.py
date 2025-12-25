d285b265e00c390306c7e6bd94c70ec6b26b6f59
MillerQuintero2001 <miquinterog@unal.edu.co>
2025-12-24 19:13:29 -0500

    Refact: 1 chapter penguin API handler input as df

--- 1_Chapter/penguin_api.py
+++ 1_Chapter/penguin_api.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 from pathlib import Path
-import warnings
-warnings.filterwarnings('ignore', category=UserWarning)
+import pandas as pd
 
 SCRIPT_DIR = Path(__file__).parent
 
@@ -15,22 +14,21 @@ app = FastAPI()
 
 # Define el modelo de datos para la petición
 class PenguinFeatures(BaseModel):
-    culmen_length_mm: float
-    culmen_depth_mm: float
+    bill_length_mm: float
+    bill_depth_mm: float
     flipper_length_mm: int
     body_mass_g: int
 
 @app.post("/predict")
 def predict(penguin: PenguinFeatures):
+    df = pd.DataFrame([penguin.model_dump()])
     
-    features = [[penguin.culmen_length_mm, penguin.culmen_depth_mm,
-                penguin.flipper_length_mm, penguin.body_mass_g]]
-    
-    prediction = model.predict(features)[0]
-    print(f"Predicted species: {prediction}")
+    predictions = model.predict(df)
+    confidence = model.predict_proba(df)
 
-    return {"predicted_species": prediction,
-            "confidence": float(max(model.predict_proba(features)[0]))}
+    return {"predicted_species": predictions.tolist(),
+            "confidence": confidence.tolist(),
+            }
 
 
 if __name__ == "__main__":
@@ -42,8 +40,8 @@ if __name__ == "__main__":
 # curl -X POST "http://localhost:8080/predict" \
 #   -H "Content-Type: application/json" \
 #   -d '{
-#     "culmen_length_mm": 39.1,
-#     "culmen_depth_mm": 18.7,
+#     "bill_length_mm": 39.1,
+#     "bill_depth_mm": 18.7,
 #     "flipper_length_mm": 181,
 #     "body_mass_g": 3750
 #   }'
